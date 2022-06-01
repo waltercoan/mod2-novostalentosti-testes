dc541cda46aacfaae52b67e69ac35662feebc07f
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-06-01 14:46:31 -0300

    exercicio 01

---
+++ src/main/java/br/univille/PlanejadorDeChurrasco.java
@@ -0,0 +1,18 @@
+package br.univille;
+
+public class PlanejadorDeChurrasco {
+    private int adulto;
+    private int crianca;
+
+    public void setAdulto(int adulto){
+        this.adulto = adulto;
+    }
+    public void setCrianca(int crianca){
+        this.crianca = crianca;
+    }
+
+    public float calculaQtdCarne(){
+        //escrever a logica do quadradinho verde
+        return 0;
+    }
+}
